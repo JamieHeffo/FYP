d3b4fd1a4320682f31579bc30f0e6f50fa7a1f5e
JamieHeffo <125657344+JamieHeffernan@users.noreply.github.com>
2024-02-20 18:31:49 +0000

    Added Pageholder Titles

--- ReactNativeApp/CookSmart/screens/Chat.tsx
+++ ReactNativeApp/CookSmart/screens/Chat.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { View, Text, Button, Alert, StyleSheet } from 'react-native';
+import { View, Text, Button, Alert, StyleSheet, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import ToDoItem from '../src/components/ToDoItem';
+import AddRecipeModal from '../src/components/AddRecipeModal';
 
-const PhotoToRecipe = ({ navigation }) => {
+const Chat = ({ navigation }) => {
 
     return (
-        <View>
-            <Text>Photo to Recipe</Text>
-            <Button title="Click Me" onPress={handleButtonClick} />
-        </View>
+        <SafeAreaView style={styles.mainView}>
+            <View style={styles.scrollView}>
+                <Text style={styles.heading}>Ask The Chef</Text>
+            </View>
+
+        </SafeAreaView>
+
+
     );
+}
+const styles = StyleSheet.create({
+    mainView: {
+        flex: 1,
+        backgroundColor: '#F887A8'
+    },
 
-};
+    scrollView: {
+        flex: 1,
+        backgroundColor: '#847DA4',
+        padding: '5%',
+    },
 
-const handleButtonClick = () => {
-    Alert.alert('Button Clicked');
-};
+    heading: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#F887A8',
+        marginTop: '5%',
+        marginBottom: '5%'
+    },
+});
 
-export default PhotoToRecipe;
+export default Chat;
--- ReactNativeApp/CookSmart/screens/PhotoRecipe.tsx
+++ ReactNativeApp/CookSmart/screens/PhotoRecipe.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import { View, Text, Button, Alert, StyleSheet } from 'react-native';
+import { View, Text, Button, Alert, StyleSheet, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
 
-const PhotoToRecipe = ({ navigation }) => {
+const PhotoRecipe = ({ navigation }) => {
 
     return (
-        <View>
-            <Text>Photo to Recipe</Text>
-            <Button title="Click Me" onPress={handleButtonClick} />
-        </View>
+        <SafeAreaView style={styles.mainView}>
+            <View style={styles.scrollView}>
+                <Text style={styles.heading}>Generate Recipe</Text>
+            </View>
+
+        </SafeAreaView>
+
+
     );
+}
+const styles = StyleSheet.create({
+    mainView: {
+        flex: 1,
+        backgroundColor: '#F887A8'
+    },
 
-};
+    scrollView: {
+        flex: 1,
+        backgroundColor: '#847DA4',
+        padding: '5%',
+    },
 
-const handleButtonClick = () => {
-    Alert.alert('Button Clicked');
-};
+    heading: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#F887A8',
+        marginTop: '5%',
+        marginBottom: '5%'
+    },
+});
 
-export default PhotoToRecipe;
+export default PhotoRecipe;
