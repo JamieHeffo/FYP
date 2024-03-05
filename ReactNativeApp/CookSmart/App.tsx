d56db13771944f2b9a28678043438d0815843383
JamieHeffo <125657344+JamieHeffernan@users.noreply.github.com>
2024-03-05 15:26:21 +0000

    Started register and login

--- ReactNativeApp/CookSmart/App.tsx
+++ ReactNativeApp/CookSmart/App.tsx
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './navigation/tabs';
+import 'react-native-url-polyfill/auto'
+import { supabase } from './supabase/supabase'
+import Auth from './src/components/Auth'
+import Account from './src/components/Accounts'
+import { View } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
 function App(): React.JSX.Element {
+  const [session, setSession] = useState<Session | null>(null)
 
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
   return (
 
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+
+      {/*       <NavigationContainer>
+
+        <Tabs />
+      </NavigationContainer> */}
+    </View>
 
 
   );
